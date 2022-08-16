e98ee6345a1d77d2202a6d0403e136af88b267c8
JCRL21 <62746469+JCRL21@users.noreply.github.com>
2022-08-16 15:51:51 +0200

    cleaning

--- Src/DataPrep.py
+++ Src/DataPrep.py
@@ -11,10 +11,19 @@ articles_df = pd.read_csv(data_path+'articles.csv')
 customer_df = pd.read_csv(data_path+'customers.csv')
 transactions_df = pd.read_csv(data_path+'transactions_train.csv')
 
+# Preprocess customer dataframe
+# check for NaN and make a subset with relevant columns
+percent_c = (customer_df.isnull().sum()/customer_df.isnull().count()*100).sort_values(ascending = False)
+df_c = customer_df[['customer_id','age', 'club_member_status']]
+df_c.dropna(subset=['age'])
 
-df_a = articles_df[['article_id', 'prod_name', 'product_type_name', 'product_group_name', 'colour_group_name', 'department_name', 'section_name']]
+# Preprocess article dataframe
+# subset relevant columns
+df_a = articles_df[['article_id','product_type_no', 'prod_name', 'product_type_name', 'product_group_name', 'colour_group_name', 'department_name', 'section_name']]
+# check for NaN
+percent_a = (df_a.isnull().sum()/df_a.isnull().count()*100).sort_values(ascending = False)
 
-
+# Preprocess transaction train dataframe
 #datetime and create a month column
 transactions_df.t_dat = pd.to_datetime(transactions_df.t_dat)
 transactions_df['month'] =  pd.DatetimeIndex(transactions_df['t_dat']).month
@@ -24,63 +33,15 @@ transactions_df.loc[(transactions_df['month'] == 12), 'season'] = 'Winter'
 transactions_df.loc[(transactions_df['month'] >= 3) & (transactions_df['month'] <=5), 'season'] = 'Spring' 
 transactions_df.loc[(transactions_df['month'] >= 6) & (transactions_df['month'] <=8),'season'] = 'Summer' 
 transactions_df.loc[(transactions_df['month'] >= 9) & (transactions_df['month'] <=11), 'season'] = 'Autumn' 
+# drop month column
+transactions_df = transactions_df.drop("month", axis=1)
+
+df_a.to_csv("Data/Preprocessed/df_a.csv")
+df_c.to_csv("Data/Preprocessed/df_c.csv")
+transactions_df.to_csv("Data/Preprocessed/df_t.csv")
 
 
-# calculate number of sold products
-sold_count = transactions_df['article_id'].value_counts()
-sold_count=sold_count.reset_index()
-sold_count.rename(columns = {"article_id":"sold_count","index":"article_id"}, inplace=True)
-
-# create a column with articles and sold count
-df_sold = df_a.merge(sold_count, on= 'article_id', how='left')
-df_sold = df_sold.sort_values(by='sold_count', ascending=False)
-# Maybe we need to drop articles not sold in the trainin period. But we could also keep to make more sale overall with recommendations.
-#df_sold.dropna(inplace=True)
-#df_sold
-
-# Lets see if there are any popular products in the collection
 
 
-top500 = df_sold.iloc[:500]
 
-pivot = pd.pivot_table(top500, index= ["prod_name"], values='sold_count', aggfunc=np.sum)
-pivot = pivot.reset_index()
-pivot = pivot.sort_values("sold_count", ascending=False)
-
-ax = sns.barplot(x="prod_name", y="sold_count", data=pivot.iloc[0:29])
-plt.setp(ax.get_xticklabels(), rotation=90)
-plt.show()
-# We could display in % sold instead of total
-
-# Popular color?
-
-pivot = pd.pivot_table(top500, index= ["colour_group_name"], values='sold_count', aggfunc=np.sum)
-pivot = pivot.reset_index()
-pivot = pivot.sort_values("sold_count", ascending=False)
-
-ax = sns.barplot(x="colour_group_name", y="sold_count", data=pivot.iloc[0:29])
-plt.setp(ax.get_xticklabels(), rotation=90)
-plt.show()
-
-
-# Er der nogen af aldersgrupperne der køber de samme ting?
-# Hvilke aldersgrupper køber mest?
-# Is membership a factor to buy special products or just more products. 
-# What are the most popular products during the different season, maybe sequential network here?
-
-# Lets look at customers
-customer_df.isnull().sum().sort_values(ascending=False)
-# there are many missing values in the memberships
-# how is the distriobution of customers in age
-sns.histplot(data=customer_df, x="age",  binwidth = 1)
-set(xlim=(0,100))
-plt.show()
-
-# We need to make age groups instead of age alone to make usefull analysis
-
-
-c_df = pd.merge(transactions_df, customer_df.drop("postal_code", axis=1), on='customer_id', how='inner')
-labels = ['teen' , 'young' , 'middle-aged' , 'senior', 'old']
-#['16-20', '20-30','30-50','50-70', '70+']
-c_df['age_groups'] = pd.cut(c_df['age'], bins=[16, 20, 30, 50, 70,99], labels=labels)
 
---
+++ Src/Dataviz.py
@@ -0,0 +1,106 @@
+import numpy as np 
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+data_path = 'Data/Preprocessed/'
+
+
+df_a = pd.read_csv(data_path+'df_a.csv')
+df_c = pd.read_csv(data_path+'df_c.csv')
+df_t = pd.read_csv(data_path+'df_t.csv')
+
+
+# Start exploration of data
+
+# calculate number of sold products
+sold_count = df_t['article_id'].value_counts()
+sold_count=sold_count.reset_index()
+sold_count.rename(columns = {"article_id":"sold_count","index":"article_id"}, inplace=True)
+
+# create a column with articles and sold count
+df_sold = df_a.merge(sold_count, on= 'article_id', how='left')
+df_sold = df_sold.sort_values(by='sold_count', ascending=False)
+# Maybe we need to drop articles not sold in the trainin period. But we could also keep to make more sale overall with recommendations.
+#df_sold.dropna(inplace=True)
+#df_sold
+
+# Lets see if there are any popular products in the collection
+
+
+top500 = df_sold.iloc[:500]
+
+pivot = pd.pivot_table(top500, index= ["prod_name"], values='sold_count', aggfunc=np.sum)
+pivot = pivot.reset_index()
+pivot = pivot.sort_values("sold_count", ascending=False)
+
+ax = sns.barplot(x="prod_name", y="sold_count", data=pivot.iloc[0:29])
+plt.setp(ax.get_xticklabels(), rotation=90)
+plt.show()
+# We could display in % sold instead of total
+
+# Popular color?
+
+pivot = pd.pivot_table(top500, index= ["colour_group_name"], values='sold_count', aggfunc=np.sum)
+pivot = pivot.reset_index()
+pivot = pivot.sort_values("sold_count", ascending=False)
+
+ax = sns.barplot(x="colour_group_name", y="sold_count", data=pivot.iloc[0:29])
+plt.setp(ax.get_xticklabels(), rotation=90)
+plt.show()
+
+
+# 4 plots with product type name in the seasons
+prod_sea = df_t.merge(df_a, on= 'article_id', how='left')\
+            .groupby(['season' , 'product_type_no' ,'product_type_name'])\
+                .agg({'product_type_no': 'count'}).rename(columns={'product_type_no': 'qty'}).reset_index()
+
+
+fig, axes = plt.subplots(2, 2, figsize=(15, 10), sharey=True)
+fig.suptitle('Products sold in the seasons')
+
+# Winter
+Winter = prod_sea.loc[prod_sea['season'] == "Winter"].sort_values(by = 'qty', ascending = False).head(10)
+sns.barplot(ax=axes[0, 0], x=Winter.product_type_name, y=Winter.qty)
+axes[0,0].set_title("Winter")
+
+# Spring
+Spring = prod_sea.loc[prod_sea['season'] == "Spring"].sort_values(by = 'qty', ascending = False).head(10)
+sns.barplot(ax=axes[0, 1], x=Spring.product_type_name, y=Spring.qty)
+axes[0,1].set_title("Spring")
+
+# Summer
+Summer = prod_sea.loc[prod_sea['season'] == "Summer"].sort_values(by = 'qty', ascending = False).head(10)
+sns.barplot(ax=axes[1, 0], x=Summer.product_type_name, y=Summer.qty)
+axes[1,0].set_title("Summer")
+
+# Autumn
+Autumn = prod_sea.loc[prod_sea['season'] == "Autumn"].sort_values(by = 'qty', ascending = False).head(10)
+
+sns.barplot(ax=axes[1, 1], x=Autumn.product_type_name, y=Autumn.qty)
+
+axes[1,1].set_title("Autumn")
+fig.set_xticklabels(rotation=65, horizontalalignment='right')
+plt.show()
+
+# Er der nogen af aldersgrupperne der køber de samme ting?
+# Hvilke aldersgrupper køber mest?
+# Is membership a factor to buy special products or just more products. 
+# What are the most popular products during the different season, maybe sequential network here?
+
+# Lets look at customers
+df_c.isnull().sum().sort_values(ascending=False)
+# there are many missing values in the memberships
+# how is the distriobution of customers in age
+sns.histplot(data=df_c, x="age",  binwidth = 1)
+set(xlim=(0,100))
+plt.show()
+
+# We need to make age groups instead of age alone to make usefull analysis
+
+
+c_df = pd.merge(df_t, df_c.drop("postal_code", axis=1), on='customer_id', how='inner')
+labels = ['teen' , 'young' , 'middle-aged' , 'senior', 'old']
+#['16-20', '20-30','30-50','50-70', '70+']
+c_df['age_groups'] = pd.cut(c_df['age'], bins=[16, 20, 30, 50, 70,99], labels=labels)
