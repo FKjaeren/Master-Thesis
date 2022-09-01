a2a89aadf592beca03c3be458d149a6909991717
JCRL21 <62746469+JCRL21@users.noreply.github.com>
2022-09-01 14:34:57 +0200

    updated with comments

--- Src/DataPrep.py
+++ Src/DataPrep.py
@@ -13,18 +13,29 @@ transactions_df = pd.read_csv(data_path+'transactions_train.csv')
 
 # Preprocess customer dataframe
 # check for NaN and make a subset with relevant columns
+customer_df
 percent_c = (customer_df.isnull().sum()/customer_df.isnull().count()*100).sort_values(ascending = False)
-df_c = customer_df[['customer_id','age', 'club_member_status']]
+
+# we cannot use FN and Active. They have a lot of NaN values. We keep the rest
+df_c = customer_df[['customer_id','age', 'club_member_status', 'postal_code', 'fashion_news_frequency']]
 df_c.dropna(subset=['age'])
 
 # Preprocess article dataframe
+articles_df
+# From the articles we see several columns with the same information. section_name, product_group_name and garment_group_name gives almost the same information just with differnet headlines.
+# we can discard the garment_group_name and product_group_name and the number associated. 
+# we can also discard all the colour codes and only keep the colour_group_name.
+# The detailed desciption is also discarded.
+# Difference between product_code and product_type_no is product_code gives the number for the same item, while product_type_no is the number for all the trousers fx. 
+
 # subset relevant columns
-df_a = articles_df[['article_id','product_type_no', 'prod_name', 'product_type_name', 'product_group_name', 'colour_group_name', 'department_name', 'section_name']]
+df_a = articles_df[['article_id','product_code','product_type_no', 'prod_name', 'product_type_name', 'colour_group_name', 'department_name', 'section_name']]
 # check for NaN
 percent_a = (df_a.isnull().sum()/df_a.isnull().count()*100).sort_values(ascending = False)
 
 # Preprocess transaction train dataframe
 #datetime and create a month column
+transactions_df
 transactions_df.t_dat = pd.to_datetime(transactions_df.t_dat)
 transactions_df['month'] =  pd.DatetimeIndex(transactions_df['t_dat']).month
 
