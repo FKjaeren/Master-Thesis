2be4e1b029225acd83597b97630924af27c29971
JCRL21 <62746469+JCRL21@users.noreply.github.com>
2022-10-03 11:20:59 +0200

    Layers and deepFM model

--- Src/deepFM.py
+++ Src/deepFM.py
@@ -1,72 +1,28 @@
 
-
-# =========================================================================
-# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =========================================================================
-
 import torch
-from torch import nn
-from fuxictr.pytorch.models import BaseModel
-from fuxictr.pytorch.layers import EmbeddingLayer, MLP_Layer, FM_Layer
+
+from Layers import FactorizationMachine, FeaturesEmbedding, FeaturesLinear, MultiLayerPerceptron
 
 
-class DeepFM(BaseModel):
-    def __init__(self, 
-                 feature_map, 
-                 model_id="DeepFM", 
-                 gpu=-1, 
-                 task="binary_classification", 
-                 learning_rate=1e-3, 
-                 embedding_dim=10, 
-                 hidden_units=[64, 64, 64], 
-                 hidden_activations="ReLU", 
-                 net_dropout=0, 
-                 batch_norm=False, 
-                 embedding_regularizer=None, 
-                 net_regularizer=None,
-                 **kwargs):
-        super(DeepFM, self).__init__(feature_map, 
-                                     model_id=model_id, 
-                                     gpu=gpu, 
-                                     embedding_regularizer=embedding_regularizer, 
-                                     net_regularizer=net_regularizer,
-                                     **kwargs)
-        self.embedding_layer = EmbeddingLayer(feature_map, embedding_dim)
-        self.fm_layer = FM_Layer(feature_map, output_activation=None, use_bias=False)
-        self.dnn = MLP_Layer(input_dim=embedding_dim * feature_map.num_fields,
-                             output_dim=1, 
-                             hidden_units=hidden_units,
-                             hidden_activations=hidden_activations,
-                             output_activation=None, 
-                             dropout_rates=net_dropout, 
-                             batch_norm=batch_norm, 
-                             use_bias=True)
-        self.output_activation = self.get_output_activation(task)
-        self.compile(kwargs["optimizer"], loss=kwargs["loss"], lr=learning_rate)
-        self.reset_parameters()
-        self.model_to_device()
-            
-    def forward(self, inputs):
+class DeepFactorizationMachineModel(torch.nn.Module):
+    """
+    A Pytorch implementation of DeepFM.
+    Reference:
+        H Guo, et al. DeepFM: A Factorization-Machine based Neural Network for CTR Prediction, 2017.
+    """
+
+    def __init__(self, field_dims, embed_dim, mlp_dims, dropout):
+        super().__init__()
+        self.linear = FeaturesLinear(field_dims)
+        self.fm = FactorizationMachine(reduce_sum=True)
+        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
+        self.embed_output_dim = len(field_dims) * embed_dim
+        self.mlp = MultiLayerPerceptron(self.embed_output_dim, mlp_dims, dropout)
+
+    def forward(self, x):
         """
-        Inputs: [X,y]
+        :param x: Long tensor of size ``(batch_size, num_fields)``
         """
-        X, y = self.inputs_to_device(inputs)
-        feature_emb = self.embedding_layer(X)
-        y_pred = self.fm_layer(X, feature_emb)
-        y_pred += self.dnn(feature_emb.flatten(start_dim=1))
-        if self.output_activation is not None:
-            y_pred = self.output_activation(y_pred)
-        return_dict = {"y_true": y, "y_pred": y_pred}
-        return return_dict
+        embed_x = self.embedding(x)
+        x = self.linear(x) + self.fm(embed_x) + self.mlp(embed_x.view(-1, self.embed_output_dim))
+        return torch.sigmoid(x.squeeze(1))
