1b67f0a6b9b2ff252f29239dd95b7ad9120a558a
FKjaeren <f.kjaer@icloud.com>
2022-11-07 16:26:21 +0100

    .

--- Src/DeepFMTraining.py
+++ Src/DeepFMTraining.py
@@ -16,222 +16,256 @@ import time
 #api = wandb.Api()
 
 log = logging.getLogger(__name__)
-#hydra.main(config_path="config", config_name='config.yaml')
-#config = DictConfig
-config = OmegaConf.load('config/config.yaml')
-print(f"configuration: \n {OmegaConf.to_yaml(config)}")
-#hparams = config.experiment
-sweep_configuration = OmegaConf.load('config/sweep_conf.yaml')
-wandb.init(project="MasterThesis", entity="frederikogjonesmaster")
-sweep_id = wandb.sweep(sweep=sweep_configuration, project="MasterThesis")
+sweep_configuration = {
+    'method': 'random',
+    'name': 'sweep',
+    'metric': {
+        'goal': 'minimize', 
+        'name': 'val_loss'
+        },
+    'parameters': {
+        'weight_decay': {'max': 0.001, 'min':0.00001},
+        'lr': {'max': 0.05, 'min': 0.0001},
+        'latent_dim1':{'max': 64, 'min':4},
+        'latent_dim2':{'max': 128, 'min':4},
+        'latent_dim3':{'max': 64, 'min':4},
+        'embed_dim':{'max': 64, 'min':4},
+        'dropout':{'max': 0.5, 'min':0.1},
+        'pos_weight':{'max': 200, 'min':3}
+    }
+}
 count = 10
-wandb.agent(sweep_id, count=count)
-#hparams = sweep_configuration
-hparams = OmegaConf.load('config/experiment/exp1.yaml')
-device = torch.device("cuda" if hparams['cuda'] else "cpu")
-#device = "cpu"
-#wandb.config = hparams
-log.info(f'hparameters:  {hparams}')
 
-class CreateDataset(Dataset):
-    def __init__(self, dataset):#, features, idx_variable):
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="MasterThesis")
 
-        self.dataset = dataset[:,0:-1]
-        self.targets = dataset[:,-1:].float()
+#@hydra.main(config_path="../config", config_name='config.yaml')
+#config = DictConfig
+#def train_model(config: DictConfig) -> None:
+def main():
+    run = wandb.init()
+    #config = OmegaConf.load('../config/config.yaml')
+    #print(f"configuration: \n {OmegaConf.to_yaml(config)}")
+    #hparams = config.experiment
+    #sweep_configuration = OmegaConf.load('../../../../config/sweep_conf.yaml')
+    wandb.init(project="MasterThesis", entity="frederikogjonesmaster")
 
-    def __len__(self):
-        return len(self.dataset)
+    #hparams = sweep_configuration
+    hparams = {'weight_decay':wandb.config.weight_decay,
+    'lr' : wandb.config.lr,
+    'cuda':False,
+    'latent_dim1':wandb.config.latent_dim1,
+    'latent_dim2':wandb.config.latent_dim2,
+    'latent_dim3':wandb.config.latent_dim3,
+    'embed_dim':wandb.config.embed_dim,
+    'dropout':wandb.config.dropout,
+    'pos_weight':wandb.config.pos_weight}
+    device = torch.device("cuda" if hparams['cuda'] else "cpu")
+    #device = "cpu"
+    #wandb.config = hparams
+    log.info(f'hparameters:  {hparams}')
 
-    def __getitem__(self, row):
-        return self.dataset[row], self.targets[row]
-    def shape(self):
-        shape_value = self.all_data.shape
-        return shape_value
+    class CreateDataset(Dataset):
+        def __init__(self, dataset):#, features, idx_variable):
+
+            self.dataset = dataset[:,0:-1]
+            self.targets = dataset[:,-1:].float()
+
+        def __len__(self):
+            return len(self.dataset)
+
+        def __getitem__(self, row):
+            return self.dataset[row], self.targets[row]
+        def shape(self):
+            shape_value = self.all_data.shape
+            return shape_value
 
 
-train_df = pd.read_csv('Data/Preprocessed/train_df.csv')
-valid_df = pd.read_csv('Data/Preprocessed/valid_df.csv')
-test_df = pd.read_csv('Data/Preprocessed/test_df.csv')
+    train_df = pd.read_csv('../Data/Preprocessed/train_df.csv')
+    valid_df = pd.read_csv('../Data/Preprocessed/valid_df.csv')
+    test_df = pd.read_csv('../Data/Preprocessed/test_df.csv')
 
-device = torch.device('cpu')
+    device = torch.device('cpu')
 
-train_tensor = torch.tensor(train_df.fillna(0).to_numpy(), dtype = torch.int)
-valid_tensor = torch.tensor(valid_df.fillna(0).to_numpy(), dtype = torch.int)
-test_tensor = torch.tensor(test_df.fillna(0).to_numpy(), dtype = torch.int)
+    train_tensor = torch.tensor(train_df.fillna(0).to_numpy(), dtype = torch.int)
+    valid_tensor = torch.tensor(valid_df.fillna(0).to_numpy(), dtype = torch.int)
+    test_tensor = torch.tensor(test_df.fillna(0).to_numpy(), dtype = torch.int)
 
-train_dataset = CreateDataset(train_tensor)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
-valid_dataset = CreateDataset(valid_tensor)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
-test_dataset = CreateDataset(test_tensor)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
+    train_dataset = CreateDataset(train_tensor)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
+    valid_dataset = CreateDataset(valid_tensor)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
+    test_dataset = CreateDataset(test_tensor)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
 
-batch_size = 1024
+    batch_size = 1024
 
-dataset_shapes = {'train_shape':train_tensor.shape,'valid_shape':valid_tensor.shape,'test_shape':test_tensor.shape}
+    dataset_shapes = {'train_shape':train_tensor.shape,'valid_shape':valid_tensor.shape,'test_shape':test_tensor.shape}
 
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, num_workers = 0, shuffle = True, drop_last = True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, num_workers = 0, shuffle = True, drop_last = True)
 
-valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size = batch_size, num_workers = 0, shuffle = True, drop_last = True)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size = batch_size, num_workers = 0, shuffle = True, drop_last = True)
 
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = batch_size, num_workers = 0, shuffle = False, drop_last = True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = batch_size, num_workers = 0, shuffle = False, drop_last = True)
 
-with open(r"Data/Preprocessed/number_uniques_dict.pickle", "rb") as input_file:
-    number_uniques_dict = pickle.load(input_file)
+    with open(r"../Data/Preprocessed/number_uniques_dict.pickle", "rb") as input_file:
+        number_uniques_dict = pickle.load(input_file)
 
-class DeepFactorizationMachineModel(torch.nn.Module):
-    """
-    A Pytorch implementation of DeepFM.
-    Reference:
-        H Guo, et al. DeepFM: A Factorization-Machine based Neural Network for CTR Prediction, 2017.
-    """
-
-    def __init__(self, field_dims, hparams, n_unique_dict, device, batch_size):
-        super().__init__()
-        mlp_dims = [hparams["latent_dim1"], hparams["latent_dim2"], hparams["latent_dim3"]]
-        #self.linear = FeaturesLinear(field_dims)
-        self.fm = FactorizationMachine(reduce_sum=True)
-        self.embedding = FeaturesEmbedding(embedding_dim = hparams["embed_dim"],num_fields=field_dims ,batch_size= batch_size, n_unique_dict=n_unique_dict, device = device)
-        self.embed_output_dim = (len(field_dims)-1) * hparams["embed_dim"]
-        self.mlp = MultiLayerPerceptron(self.embed_output_dim, mlp_dims, hparams["dropout"])
-
-    def forward(self, x):
+    class DeepFactorizationMachineModel(torch.nn.Module):
         """
-        :param x: Long tensor of size ``(batch_size, num_fields)``
+        A Pytorch implementation of DeepFM.
+        Reference:
+            H Guo, et al. DeepFM: A Factorization-Machine based Neural Network for CTR Prediction, 2017.
         """
-        embed_x = self.embedding(x)
-        if(torch.isnan(embed_x).sum() > 0):
-            print("Values with nan in embedding output: ",embed_x[torch.isnan(embed_x)])
-        if(torch.isnan(self.fm(embed_x)).sum() > 0):
-            print("Values with nan in fm output: ",self.fm(embed_x)[torch.isnan(self.fm(embed_x))])
-        if(torch.isnan(self.mlp(embed_x.view(-1, self.embed_output_dim))).sum() > 0):
-            print("Values with nan in mlp output: ",self.mlp(embed_x.view(-1, self.embed_output_dim))[torch.isnan(self.mlp(embed_x.view(-1, self.embed_output_dim)))])
-        #x = self.linear(x) + self.fm(embed_x) + self.mlp(embed_x.view(-1, self.embed_output_dim))
-        x = self.fm(embed_x) + self.mlp(embed_x.view(-1, self.embed_output_dim))
-        #x = self.mlp(embed_x.view(-1, self.embed_output_dim))
-        if(torch.isnan(torch.sigmoid(x.squeeze(1))).sum() > 0):
-            print("Values with nan in sigmoid output: ",torch.sigmoid(x.squeeze(1))[torch.isnan(torch.sigmoid(x.squeeze(1)))])
-        return torch.sigmoid(x.squeeze(1))
-    def Reccomend_topk(x, k):
-        item_idx = torch.topk(x,k)
-        return item_idx
+
+        def __init__(self, field_dims, hparams, n_unique_dict, device, batch_size):
+            super().__init__()
+            mlp_dims = [hparams["latent_dim1"], hparams["latent_dim2"], hparams["latent_dim3"]]
+            #self.linear = FeaturesLinear(field_dims)
+            self.fm = FactorizationMachine(reduce_sum=True)
+            self.embedding = FeaturesEmbedding(embedding_dim = hparams["embed_dim"],num_fields=field_dims ,batch_size= batch_size, n_unique_dict=n_unique_dict, device = device)
+            self.embed_output_dim = (len(field_dims)-1) * hparams["embed_dim"]
+            self.mlp = MultiLayerPerceptron(self.embed_output_dim, mlp_dims, hparams["dropout"])
+
+        def forward(self, x):
+            """
+            :param x: Long tensor of size ``(batch_size, num_fields)``
+            """
+            embed_x = self.embedding(x)
+            if(torch.isnan(embed_x).sum() > 0):
+                print("Values with nan in embedding output: ",embed_x[torch.isnan(embed_x)])
+            if(torch.isnan(self.fm(embed_x)).sum() > 0):
+                print("Values with nan in fm output: ",self.fm(embed_x)[torch.isnan(self.fm(embed_x))])
+            if(torch.isnan(self.mlp(embed_x.view(-1, self.embed_output_dim))).sum() > 0):
+                print("Values with nan in mlp output: ",self.mlp(embed_x.view(-1, self.embed_output_dim))[torch.isnan(self.mlp(embed_x.view(-1, self.embed_output_dim)))])
+            #x = self.linear(x) + self.fm(embed_x) + self.mlp(embed_x.view(-1, self.embed_output_dim))
+            x = self.fm(embed_x) + self.mlp(embed_x.view(-1, self.embed_output_dim))
+            #x = self.mlp(embed_x.view(-1, self.embed_output_dim))
+            if(torch.isnan(torch.sigmoid(x.squeeze(1))).sum() > 0):
+                print("Values with nan in sigmoid output: ",torch.sigmoid(x.squeeze(1))[torch.isnan(torch.sigmoid(x.squeeze(1)))])
+            return torch.sigmoid(x.squeeze(1))
+        def Reccomend_topk(x, k):
+            item_idx = torch.topk(x,k)
+            return item_idx
 
 
 
-#embedding_dim = 16
-DeepFMModel = DeepFactorizationMachineModel(field_dims = train_df.columns, hparams = hparams, n_unique_dict = number_uniques_dict, device = device, batch_size=batch_size)
-optimizer = torch.optim.Adam(DeepFMModel.parameters(), weight_decay=hparams["weight_decay"], lr = hparams["lr"])
-#pos_weight = train_df.target.value_counts()[0] / train_df.target.value_counts()[1]
-#pos_weight = 100000
-loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight = torch.tensor(hparams["pos_weight"]))
-#loss_fn = torch.nn.BCEWithLogitsLoss()
-def init_weights(m):
-    if isinstance(m, nn.Linear):
-        nn.init.xavier_uniform_(m.weight)
-        #m.weight.data.fill_(0.0)
-        m.bias.data.fill_(0.01)
-    elif isinstance(m, nn.Embedding):
-        m.weight.data.normal_(mean=0.0, std=1.0)
-        #m.weight.data.fill_(0.0)
-        if m.padding_idx is not None:
-            m.weight.data[m.padding_idx].zero_()
+    #embedding_dim = 16
+    DeepFMModel = DeepFactorizationMachineModel(field_dims = train_df.columns, hparams = hparams, n_unique_dict = number_uniques_dict, device = device, batch_size=batch_size)
+    optimizer = torch.optim.Adam(DeepFMModel.parameters(), weight_decay=hparams["weight_decay"], lr = hparams["lr"])
+    #pos_weight = train_df.target.value_counts()[0] / train_df.target.value_counts()[1]
+    #pos_weight = 100000
+    loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight = torch.tensor(hparams["pos_weight"]))
+    #loss_fn = torch.nn.BCEWithLogitsLoss()
+    def init_weights(m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            #m.weight.data.fill_(0.0)
+            m.bias.data.fill_(0.01)
+        elif isinstance(m, nn.Embedding):
+            m.weight.data.normal_(mean=0.0, std=1.0)
+            #m.weight.data.fill_(0.0)
+            if m.padding_idx is not None:
+                m.weight.data[m.padding_idx].zero_()
 
 
-DeepFMModel.apply(init_weights)
-wandb.watch(DeepFMModel, log_freq=500)
+    DeepFMModel.apply(init_weights)
+    wandb.watch(DeepFMModel, log_freq=500)
 
-num_epochs = 2
-res = []
-Loss_list = []
-Valid_Loss_list = []
-Best_loss = np.infty
-for epoch in range(1,num_epochs+1):
-    start = time.time()
+    num_epochs = 2
+    res = []
+    Loss_list = []
+    Valid_Loss_list = []
+    Best_loss = np.infty
+    for epoch in range(1,num_epochs+1):
+        start = time.time()
 
-    print(epoch)
-    running_loss = 0.
-    running_loss_val = 0.
-    epoch_loss = []
-    DeepFMModel.train()
+        print(epoch)
+        running_loss = 0.
+        running_loss_val = 0.
+        epoch_loss = []
+        DeepFMModel.train()
 
-    for batch, (X,y) in enumerate(train_loader):
-        # Zero your gradients for every batch!
-        optimizer.zero_grad()
-        #
-        dataset = X.to(device)
-        # Make predictions for this batch
-        outputs = DeepFMModel(dataset)
-        if(torch.isnan(X).sum() > 0):
-            print("SE her Values with nan in X: ",X[torch.isnan(X)])
-        if(torch.isnan(outputs).sum() > 0):
-            print("Values with nan in outputs: ",outputs[torch.isnan(outputs)])
-            print("And the batch is: ", batch)
-        # Compute the loss and its gradients
+        for batch, (X,y) in enumerate(train_loader):
+            # Zero your gradients for every batch!
+            optimizer.zero_grad()
+            #
+            dataset = X.to(device)
+            # Make predictions for this batch
+            outputs = DeepFMModel(dataset)
+            if(torch.isnan(X).sum() > 0):
+                print("SE her Values with nan in X: ",X[torch.isnan(X)])
+            if(torch.isnan(outputs).sum() > 0):
+                print("Values with nan in outputs: ",outputs[torch.isnan(outputs)])
+                print("And the batch is: ", batch)
+            # Compute the loss and its gradients
 
-        loss = loss_fn(outputs,y.squeeze().to(device))
-        loss.backward()
-        # Adjust learning weights
-        optimizer.step()
-        running_loss += loss 
-            # Gather data and report
-        epoch_loss.append(loss.item())
-        #if(batch % 500 == 0):
-        #    print(' Train batch {} loss: {}'.format(batch, np.mean(epoch_loss)))
-        wandb.log({"train_loss": loss})
-    if(epoch % 1 == 0):
-        print(' Train epoch {} loss: {}'.format(epoch, np.mean(epoch_loss)))
-        log.info(f"at epoch: {epoch} the Training loss is : {running_loss/len(train_loader)}") 
+            loss = loss_fn(outputs,y.squeeze().to(device))
+            loss.backward()
+            # Adjust learning weights
+            optimizer.step()
+            running_loss += loss 
+                # Gather data and report
+            epoch_loss.append(loss.item())
+            #if(batch % 500 == 0):
+            #    print(' Train batch {} loss: {}'.format(batch, np.mean(epoch_loss)))
+            wandb.log({"train_loss": loss})
+        if(epoch % 1 == 0):
+            print(' Train epoch {} loss: {}'.format(epoch, np.mean(epoch_loss)))
+            log.info(f"at epoch: {epoch} the Training loss is : {running_loss/len(train_loader)}") 
 
-        epoch_loss.append(loss.item())
+            epoch_loss.append(loss.item())
 
 
-    epoch_loss_value = np.mean(epoch_loss)
-    Loss_list.append(epoch_loss_value)
-    DeepFMModel.eval()
-    epoch_valid_loss = []
-    for batch, (X_valid,y_valid) in enumerate(valid_loader):
-        outputs = DeepFMModel(X_valid)
-        loss_val = loss_fn(outputs,y_valid.squeeze())
-        epoch_valid_loss.append(loss_val.item())
-        running_loss_val += loss_val 
+        epoch_loss_value = np.mean(epoch_loss)
+        Loss_list.append(epoch_loss_value)
+        DeepFMModel.eval()
+        epoch_valid_loss = []
+        for batch, (X_valid,y_valid) in enumerate(valid_loader):
+            outputs = DeepFMModel(X_valid)
+            loss_val = loss_fn(outputs,y_valid.squeeze())
+            epoch_valid_loss.append(loss_val.item())
+            running_loss_val += loss_val 
 
-        wandb.log({"val_loss": loss_val})
+            wandb.log({"val_loss": loss_val})
 
-    if(epoch % 1 == 0):
-        print(' Valid epoch {} loss: {}'.format(epoch, np.mean(epoch_valid_loss)))
-        log.info(f"at epoch: {epoch} the Validation loss is : {running_loss_val/len(valid_loader)}") 
+        if(epoch % 1 == 0):
+            print(' Valid epoch {} loss: {}'.format(epoch, np.mean(epoch_valid_loss)))
+            log.info(f"at epoch: {epoch} the Validation loss is : {running_loss_val/len(valid_loader)}") 
 
-    epoch_valid_loss_value = np.mean(epoch_valid_loss)
-    Valid_Loss_list.append(epoch_valid_loss_value)
-    if(epoch_valid_loss_value < Best_loss):
-        best_model = copy.deepcopy(DeepFMModel)
-        Best_loss = epoch_valid_loss_value
+        epoch_valid_loss_value = np.mean(epoch_valid_loss)
+        Valid_Loss_list.append(epoch_valid_loss_value)
+        if(epoch_valid_loss_value < Best_loss):
+            best_model = copy.deepcopy(DeepFMModel)
+            Best_loss = epoch_valid_loss_value
 
-    end = time.time()
-    res.append(end - start)
-res = np.array(res)
-log.info(f'Timing: {np.mean(res)} +- {np.std(res)}')
-#PATH = 'Models/DeepFM_model.pth'
-#torch.save(best_model, PATH)
+        end = time.time()
+        res.append(end - start)
+    res = np.array(res)
+    log.info(f'Timing: {np.mean(res)} +- {np.std(res)}')
+    #PATH = 'Models/DeepFM_model.pth'
+    #torch.save(best_model, PATH)
 
-print("finished training")
-print("Loss list = ", Loss_list)
-##test:
-# 1 iteration:
+    print("finished training")
+    print("Loss list = ", Loss_list)
+    ##test:
+    # 1 iteration:
 
-dataiter = iter(test_loader)
-X, y = next(dataiter)
+    dataiter = iter(test_loader)
+    X, y = next(dataiter)
 
-output = DeepFMModel(X)
+    output = DeepFMModel(X)
 
-predictions = []
-for i in output:
-    if i < 0.5:
-        predictions.append(0)
-    else:
-        predictions.append(1)
+    predictions = []
+    for i in output:
+        if i < 0.5:
+            predictions.append(0)
+        else:
+            predictions.append(1)
 
-print("The output of the model is: ", predictions)
+    print("The output of the model is: ", predictions)
 
-print("The true labels are: ", y)
+    print("The true labels are: ", y)
 
-print("The accuracy of the model on 1 iterations is:", (1-(torch.sum(y.squeeze() - torch.tensor(predictions, dtype = torch.int)).item())/len(y))*100,"%")
+    print("The accuracy of the model on 1 iterations is:", (1-(torch.sum(y.squeeze() - torch.tensor(predictions, dtype = torch.int)).item())/len(y))*100,"%")
 
+#if __name__ == '__main__':
+#    train_model()
+wandb.agent(sweep_id, function = main,count=count)
--- Src/FindRelevantCustomersItems.py
+++ Src/FindRelevantCustomersItems.py
@@ -10,14 +10,16 @@ item_agg_subset = item_agg[item_agg['t_dat']>'2020-03-22']
 
 cust_agg_subset = cust_agg[cust_agg['t_dat']>'2020-03-22']
 
-cust_agg_subset_v2 = cust_agg_subset[(cust_agg_subset['t_dat']>'2020-06-22')]
-cust_agg_subset_v2 = cust_agg_subset_v2[cust_agg_subset_v2['customer_id']>4]
+cust_agg_subset_v2 = cust_agg_subset[(cust_agg_subset['t_dat']>'2020-07-22')]
+cust_agg_subset_v2 = cust_agg_subset_v2[cust_agg_subset_v2['customer_id']>5]
 cust_agg_subset_v2 = cust_agg_subset_v2.rename(columns = {'customer_id':'purchase_count'}).reset_index()
-item_agg_subset2 = item_agg[item_agg['article_id']>=10]
+item_agg_subset2 = item_agg[item_agg['article_id']>=15]
 item_agg_subset2 = item_agg_subset2.rename(columns = {'article_id':'purchase_count'}).reset_index()
 
 transaction_subset = df[df['customer_id'].isin(cust_agg_subset_v2.customer_id)]
 transaction_subset = transaction_subset[transaction_subset['article_id'].isin(item_agg_subset2.article_id)]
+
+transaction_subset = transaction_subset[transaction_subset['t_dat']>'2019-09-22']
 
 transaction_subset.to_csv('Data/Raw/transactions_train_subset.csv', index = False)
 
