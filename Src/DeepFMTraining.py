c1c01112559ef92902c78fe3fc9a4d98be38b058
JCRL21 <62746469+JCRL21@users.noreply.github.com>
2022-11-07 12:25:42 +0100

    gg

--- Src/DeepFMTraining.py
+++ Src/DeepFMTraining.py
@@ -8,7 +8,7 @@ import pickle
 import copy
 from Src.Layers import FactorizationMachine, FeaturesEmbedding, MultiLayerPerceptron#, FeaturesLinear
 import hydra
-from omegaconf import DictConfig, OmegaConf
+#from omegaconf import DictConfig, OmegaConf
 import logging
 import wandb
 import time
