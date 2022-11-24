fced8a14b83ff59812f77ce432c025f5a3c8bd66
JCRL21 <62746469+JCRL21@users.noreply.github.com>
2022-11-24 13:06:49 +0100

    forsøg på at lave med deres dataloa

--- Src/Datadrift_exp.py
+++ Src/Datadrift_exp.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 from torch import nn
 import copy
 import pandas as pd
+import pytorch_lightning as pl
 
 valid_df = pd.read_csv('Data/Preprocessed/valid_df_subset.csv')
 
@@ -53,16 +54,103 @@ def corruption_function(x: torch.Tensor):
     return torchdrift.data.functional.gaussian_blur(x, severity=2)
 
 
-inputs, _ = next(iter(valid_loader))
-inputs_ood = corruption_function(inputs)
+
+inputs, _  = next(iter(valid_loader))
+
+def collate_fn(batch):
+    batch = torch.utils.data._utils.collate.default_collate(batch)
+    batch = ((batch[0]), *batch[1:])
+    return batch
+col = collate_fn(inputs)
+inputs_ood = corruption_function(col)
+
+
+
 
 feature_extractor = copy.deepcopy(model)
 
-feature_extractor.embed_x = torch.nn.Identity()
+#feature_extractor.embedding.fc = torch.nn.Identity()
 
-drift_detector = torchdrift.detectors.KernelMMDDriftDetector()
-torchdrift.utils.fit(valid_loader, feature_extractor, drift_detector, num_batches=1)
-drift_detection_model = torch.nn.Sequential(
-    feature_extractor,
-    drift_detector
-)
+
+
+
+class OurDataModule(pl.LightningDataModule):
+    def __init__(self, parent: Optional['OurDataModule']=None, additional_transform=None):
+        if parent is None:
+            
+            self.val_dataset = CreateDataset(valid_tensor)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
+
+            
+            self.val_batch_size = 128
+            self.additional_transform = None
+        else:
+            self.val_dataset = parent.val_dataset
+            self.val_batch_size = parent.val_batch_size
+            self.additional_transform = additional_transform
+        if additional_transform is not None:
+            self.additional_transform = additional_transform
+
+        self.prepare_data()
+        self.setup('fit')
+        self.setup('test')
+
+    def setup(self, typ):
+        pass
+
+    def collate_fn(self, batch):
+        batch = torch.utils.data._utils.collate.default_collate(batch)
+        if self.additional_transform:
+            batch = (self.additional_transform(batch[0]), *batch[1:])
+        return batch
+    
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.val_batch_size,
+                                           num_workers=4, shuffle=False, collate_fn=self.collate_fn)
+
+    def default_dataloader(self, batch_size=None, num_samples=None, shuffle=True):
+        dataset = self.val_dataset
+        if batch_size is None:
+            batch_size = self.val_batch_size
+        replacement = num_samples is not None
+        if shuffle:
+            sampler = torch.utils.data.RandomSampler(dataset, replacement=replacement, num_samples=num_samples)
+        else:
+            sampler = None
+        return torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=sampler,
+                                           collate_fn=self.collate_fn)
+
+
+datamodule = OurDataModule()
+
+
+
+
+
+
+
+
+
+
+
+detector = torchdrift.detectors.ks.KSDriftDetector()
+
+
+
+odd_input = (inputs + torch.randn(inputs.shape[0], inputs.shape[1])).type(torch.int32)
+
+odd_dataset = CreateDataset(odd_input)#, features=['price','age','colour_group_name','department_name'],idx_variable=['customer_id'])
+
+
+odd_loader = torch.utils.data.DataLoader(odd_dataset, batch_size = batch_size, num_workers = 0, shuffle = True, drop_last = True)
+
+
+#odd_input = torchdrift.data.functional.gaussian_noise(inputs, severity= ) 
+
+ood_ratio = 0.8
+sample_size = 1
+experiment = torchdrift.utils.DriftDetectionExperiment(detector, feature_extractor.embedding, ood_ratio=ood_ratio, sample_size=sample_size)
+experiment.post_training(valid_loader)
+auc, (fp, tp) = experiment.evaluate(valid_loader, odd_loader)
+pyplot.plot(fp, tp)
+pyplot.title(label=f'{detector}, {red}\n$p_{{ood}}$={ood_ratio:.2f}, N={sample_size} AUC={auc:.3f}')
+pyplot.show()
